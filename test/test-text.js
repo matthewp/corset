dca97e2cc59c5cad91eb996627d15dc3154fa4ff
Matthew Phillips <matthew@phillipsoft.com>
2022-02-28 07:17:55 -0500

    Join arrays with text
    Fixes #105

--- test/test-text.js
+++ test/test-text.js
@@ -15,3 +15,23 @@ QUnit.test('Sets the textContent if an element', assert => {
   bindings.update(root);
   assert.equal(root.firstElementChild.textContent, 'bar');
 });
+
+QUnit.test("Providing multiple values results in them being joined", assert => {
+  let root = document.createElement('main');
+  root.innerHTML = `<div id="app"><div id="one"></div><div id="two"></div></div>`;
+  sheet`
+    #app {
+      --val: "three" "four";
+    }
+
+    #one {
+      text: "one" "two";
+    }
+
+    #two {
+      text: var(--val);
+    }
+  `.update(root);
+  assert.equal(root.querySelector('#one').textContent, 'onetwo');
+  assert.equal(root.querySelector('#two').textContent, 'threefour');
+})
