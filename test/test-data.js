5d3b4613d4595d69698dc8b3a5fef5080d0f45cc
Matthew Phillips <matthew@matthewphillips.info>
2022-07-15 08:06:12 -0400

    Prevent rendering changes when a var is unresolvable (#150)
    * Prevent rendering changes when a var is unresolvable
    
    * Adds a changeset

--- test/test-data.js
+++ test/test-data.js
@@ -49,3 +49,27 @@ QUnit.test('Changes cause selectors to update', assert => {
   bindings.update(root);
   assert.equal(root.firstElementChild.classList.contains('works'), true);
 });
+
+QUnit.test('Selector bound by another binding can be toggled on and off', assert => {
+  let root = document.createElement('main');
+  root.innerHTML = `<div id="app"></div>`;
+  function run(value) {
+    return sheet`
+      #app {
+        class-toggle: one ${value};
+      }
+
+      .one {
+        --value: "two";
+        data: one var(--value);
+      }
+    `;
+  }
+  let app = root.firstElementChild;
+  run(true).update(root);
+  assert.equal(app.dataset.one, 'two');
+  run(false).update(root);
+  assert.equal(app.dataset.one, undefined);
+  run(true).update(root);
+  assert.equal(app.dataset.one, 'two');
+});
