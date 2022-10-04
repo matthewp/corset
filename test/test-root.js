267860f43fa00fd823280e972054e811310535cf
Matthew Phillips <matthew@matthewphillips.info>
2022-10-04 08:06:58 -0400

    Allow targeting the root element via :scope

--- test/test-root.js
+++ test/test-root.js
@@ -18,3 +18,21 @@ QUnit.test(':root selector targets the root element', assert => {
   template(false).update(root);
   assert.equal(root.classList.contains('works'), false);
 });
+
+QUnit.only(':scope selector targets the root element', assert => {
+  let root = document.createElement('main');
+
+  function template(value) {
+    return sheet`
+      :scope {
+        class-toggle: works ${value};
+      }
+    `;
+  }
+
+  template(true).update(root);
+  console.log(root);
+  assert.equal(root.classList.contains('works'), true);
+  template(false).update(root);
+  assert.equal(root.classList.contains('works'), false);
+});
